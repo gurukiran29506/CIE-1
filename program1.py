261fb6a7c3504d36f4332ba481710627cc497a1e
gurukiran29506 <gurukiranhippargi@gmail.com>
2025-10-18 09:40:33 +0530

    saving file

--- program1.py
+++ program1.py
@@ -1,7 +1,5 @@
 #Declaring variables
 
 num=int(input("Enter a number: "))
-print(f"Multiplication table for {num}:")
+print(f"Cube of {num} is {num**3}")
 
-for i in range(1, 11):
-    print(f"{num} x {i} = {num * i}")
